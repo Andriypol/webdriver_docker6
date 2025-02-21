30b3d4ca59131b396df4ad5d2b217a472e9bdd53
turtulian <turtulian@gmail.com>
2025-02-21 22:11:39 +0200

    11 commit 21.02

--- wdio.chrome.conf.js
+++ wdio.chrome.conf.js
@@ -3,10 +3,10 @@ import { config } from './wdio.conf.js';
 export const chromeConfig = {
     ...config,
     capabilities: [{
-        maxInstances: 2,
+        maxInstances: 3,
         browserName: 'chrome',
         'goog:chromeOptions': {
-            args: ['--headless', '--disable-gpu', '--window-size=1920,1080', '--no-sandbox'],
+            args: ['--headless', '--disable-gpu', '--window-size=1920,1080'],
         },
     }],
     services: [],
